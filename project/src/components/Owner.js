694cd8c88fef244a0a21f5c038213318710d451f
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-01-31 14:39:58 -0800

    Adds Image Uploader to Owner component

--- project/src/components/Owner.js
+++ project/src/components/Owner.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
 
 const Owner = () => {
   const [owner, setOwner] = useState({
@@ -7,13 +6,30 @@ const Owner = () => {
     Location: '',
     Price: '',
   })
+  const [image, setImage] = useState([])
+  const [imageURLs, setImageURLs] = useState([])
+
+  useEffect(() => {
+    if (image.length < 1) return
+    const newImageURLs = []
+    imageURLs.forEach((images) => newImageURLs.push(URL.createObjectURL(image)))
+    setImageURLs(newImageURLs)
+  }, [image])
+
+  const onImageChange = (e) => {
+    setImages([...e.target.files])
+  }
 
   return (
     <div>
       <h1> Owners Profile</h1>
+      <input type="file" multiple accept="image/*" onChange={onImageChange} />
+        {imageURLs.map(imageSRC => <img src={imageSrc} />)}
+
       <p>Name: state.name</p>
       <p>Location: state.location </p>
       <p>Add Item Link</p>
+      <input type="file" multiple accept="image/*" onChange={onImageChange} />
     </div>
   )
 }
