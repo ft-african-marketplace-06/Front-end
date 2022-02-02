aa488089f5ad2e18a7104be208f288f5b1d2a188
Gaston Irby III <gaston@Gastons-iMac.hsd1.wa.comcast.net>
2022-02-02 09:57:06 -0800

    routed login

--- project/src/components/NavBar.js
+++ project/src/components/NavBar.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom'; 
 
-import Login from './Login.js'
+
+import Login from './Login.js';
 
 const NavBar = () => {
     return(
         <div className='NavBar'>
-            <Login>Login</Login>
+            <Router>
+                <Link to="/login">Login</Link>
+                <Route exact path= '/login' component={Login}/>      
+                <Link to='/'>Home</Link>
+            </Router>
             <button>Search</button>
         </div>
     )
