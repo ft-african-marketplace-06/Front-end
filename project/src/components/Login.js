77537bc9cd4b10247d07baa88f65420db7616236
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-02-02 12:34:37 -0800

    AxioswithAuth Utils folder created

--- project/src/components/Login.js
+++ project/src/components/Login.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react'
+import  axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 const Login = () => {
 
   const [credentials, setCredentials] = useState({usename:'', password:''})
+  const handlePush = useHistory()
 
 const handleChange =(e) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value })
 }
 const handleSubmit = (e) => {
     e.preventDefault()
-}
+//    axios.post('http://localhost:3000/api/auth/login', credentials)
+//    .then(resp=> {
+//        handlePush.push('/owner')
+//        console.log(resp)
+       
+//    },[])
+// }
     return (
         <div>
             <div className='container'>
