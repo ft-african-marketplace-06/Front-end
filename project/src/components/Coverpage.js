bea93db8e9373eebe6c1c270c44bff8a0c5925af
Mina <minasoha@yahoo.com>
2022-02-03 18:55:33 -0500

    added coverpage/ fixed routing

---
+++ project/src/components/Coverpage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Coverpage = () => {
+ return (
+  <div>
+   <h1 className='title'>Welcome to Our Shop</h1>
+  </div>
+ );
+};
--- project/src/components/NavBar.js
+++ project/src/components/NavBar.js
@@ -1,20 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import Login from "./Login.js";
 const NavBar = () => {
+ const { push } = useHistory();
+ const handleClick = (e) => {
+  e.preventDefault();
+  push("/");
+ };
  return (
   <div className='NavBar'>
    <Router>
-    <Link to='/login'>Login</Link>
+    <Link className='link' to='/login'>
+     Login
+    </Link>
     <Route exact path='/login' component={Login} />
-    <Link to='/'>Home</Link>
+    <Link onClick={handleClick} className='link' to='/'>
+     Home
+    </Link>
    </Router>
 
-   <Link to='/shop'>Shop</Link>
-   <Link to='/account'>Account</Link>
+   <Link className='link' to='/shop'>
+    Shop
+   </Link>
+   <Link className='link' to='/account'>
+    Account
+   </Link>
 
-   <Link to='/register'>Register</Link>
+   <Link className='link' to='/register'>
+    Register
+   </Link>
    <button>Search</button>
   </div>
  );
