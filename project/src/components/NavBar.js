4ae3459dfe1900fddb9f787663e544c37411241c
Mina <minasoha@yahoo.com>
2022-02-03 18:16:30 -0500

    fixing navBar

--- project/src/components/NavBar.js
+++ project/src/components/NavBar.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 
-import Login from './Login.js'
-const NavBar = ()=> {
-  return (
-    <div className="NavBar">
-      <Router>
-        <Link to="/login">Login</Link>
-        <Route exact path="/login" component={Login} />
-        <Link to="/">Home</Link>
-      </Router>
-       
-        <Link to='/shop'>Shop</Link>
-        <Link to='/account'>Account</Link>
-      
-        <Link to='/register'>Register</Link>
-      <button>Search</button>
-       
-    </div>
-  )
-}
+import Login from "./Login.js";
+const NavBar = () => {
+ return (
+  <div className='NavBar'>
+   <Router>
+    <Link to='/login'>Login</Link>
+    <Route exact path='/login' component={Login} />
+    <Link to='/'>Home</Link>
+   </Router>
 
-export default NavBar
+   <Link to='/shop'>Shop</Link>
+   <Link to='/account'>Account</Link>
+
+   <Link to='/register'>Register</Link>
+   <button>Search</button>
+  </div>
+ );
+};
+
+export default NavBar;
