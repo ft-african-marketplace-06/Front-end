aa9f9f08a90b9332192c48dc6682a4439705da22
Mina <minasoha@yahoo.com>
2022-02-03 18:22:29 -0500

    fixing navBar

--- project/src/components/NavBar.js
+++ project/src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 import Login from "./Login.js";
 const NavBar = () => {
